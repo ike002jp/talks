035943e44a9931af1433395fffe904eb1f3f541f
Kyohei Ikematsu <kyohei.ikematsu@dena.com>
2014-06-27 10:32:16 +0900

    first commit

---
+++ 2014/0627_dotcmd/sample.pl
@@ -0,0 +1,40 @@
+use utf8;
+use Data::Dumper;
+
+
+sub very_bad_func {
+    my ($arg1, $arg2, $arg3) = @_;
+    
+    # ##### ##### Needless Comment ##### #####
+    # This function takes some ...
+    # The returned value is ....
+    
+    # Important Comment !!!!!!
+    
+    # ##### ##### Needless Comment ##### #####
+    # The folloing is the ....
+    
+    print "Start Count Up. ¥n"
+    my $tmp = 0;
+    Very::Important::tmp($tmp);
+
+
+    print "Count Up. ¥n"
+    $tmp += 100;
+    Very::Important::tmp($tmp);
+
+
+    print "Count Up. ¥n";
+    $tmp += 10;
+    Very::Important::tmp($tmp);
+
+
+    # ##### ##### Needless Comment ##### #####
+    # these process is for the ....
+    # the result is used for ...
+    # after that ...
+
+}
+
+
+
